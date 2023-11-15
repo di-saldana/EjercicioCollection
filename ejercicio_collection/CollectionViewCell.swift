5f61f9b1931c79cfe6f3b01b5c30ed982d69a931
di-saldana <dsl42@alu.ua.es>
2023-11-15 19:37:05 +0100

    Ejercicio 2

---
+++ ejercicio_collection/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  ejercicio_collection
+//
+//  Created by Dianelys Saldaña on 11/15/23.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
